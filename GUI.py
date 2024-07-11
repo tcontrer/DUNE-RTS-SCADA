2563ccea527924f576f474e16dc4381135579073
volson1 <olsonvm2007@yahoo.com>
2024-07-11 16:04:23 -0500

    Alert and background color change when light curtain is tripped

--- GUI.py
+++ GUI.py
@@ -8,6 +8,23 @@ import threading
 
 # * Creates a user interface that displays the current state and has buttons to start and stop the robot
 # * Must be passed a RTS state machine
+
+# * Methods:
+# __init__: Creates the GUI in its inital state with start and stop button and label to display current state.
+# Runs check state in seperate thread to consitantly update the GUI
+# checkState: Runs every second until the GUI is closed. Calls update_label, update_buttons, and update_background
+# on_closing: Runs when the GUI is closed. Properly destroys the window.
+# start_robot: Cycles through to the starting then the started state
+# stop_robot: Cycles through to the stopping then the stopped state
+# update_label: Changes the color and text of the label to match the current state
+# update_buttons: Disables the buttons in states where they shouldn't be pressed and resets them when they can be pressed
+# update_background: Changes the background to orange when in an fault state and resets it when not
+# cycle: Cycles the state machine and updates the label
+
+# * Variables:
+# sm: Assigned to the state machine passed into the class. Used to access and transition state.
+# flag: Boolean that tracks if the GUI has been closed
+
 class GUI:
      def __init__(self, sm: RTSSM.RTSMachine) -> None:       
           self.sm: RTSSM.RTSMachine = sm
@@ -33,12 +50,12 @@ class GUI:
 
           # * A button to start the robot (can't be pressed if stopping)
           # * Calls the start_robot() method
-          self.startbtn: tk.Button = tk.Button(self.frame, text="Start", font=('Helvetica', 18), command=self.start_robot)
+          self.startbtn: tk.Button = tk.Button(self.frame, bg="#4C8C2B", text="Start", font=('Helvetica', 18), command=self.start_robot)
           self.startbtn.grid(row=0, column=0, padx=10, pady=10)
 
           # * Button to stop the robot (can only be pressed if started)
           # * Calls the stop_robot() method
-          self.stopbtn: tk.Button = tk.Button(self.frame, bg="#FF0000", text="Stop", font=('Helvetica', 18), command=self.stop_robot)
+          self.stopbtn: tk.Button = tk.Button(self.frame, bg="#8A2A2B", text="Stop", font=('Helvetica', 18), command=self.stop_robot)
           self.stopbtn.grid(row=0, column=1, padx=10, pady=10)
 
           # Create a new thread where the gui will constantly check the state
@@ -60,6 +77,8 @@ class GUI:
                else:
                     print(self.sm.current_state.id)
                     self.update_label()
+                    self.update_buttons()
+                    self.update_background()
                     
 
      # * When window is closed destroies the GUI
@@ -72,7 +91,8 @@ class GUI:
      # * Changes the state to started unless the robot is stopping (run by start button)
      def start_robot(self):
           self.sm.GUIidle = False
-          print("GUI active")
+          # // USED FOR DEBUGGING print("GUI active")
+          print("Start button clicked")
           if(self.sm.current_state.id == "stopped") | (self.sm.current_state.id == "ground"):
               self.cycle()
               time.sleep(1)
@@ -85,12 +105,13 @@ class GUI:
               messagebox.showerror(message="The robot can't be started because it is stopping.")
 
           self.sm.GUIidle = True
-          print("GUI idle")
+          # // USED FOR DEBUGGING print("GUI idle")
 
      # * Changes the state to stopped when currently started (run by stop button)
      def stop_robot(self):
           self.sm.GUIidle = False
-          print("GUI active")
+          # // USED FOR DEBUGGING print("GUI active")
+          print("Stop button clicked")
           if(self.sm.current_state.id == "started"):
               self.cycle()
               time.sleep(1)
@@ -118,6 +139,33 @@ class GUI:
           else:
                self.stopbtn: tk.Button = tk.Button(self.frame, bg="#FF0000", text="Stop", font=('Arial', 18), command=self.stop_robot)
                self.stopbtn.grid(row=0, column=1, padx=10, pady=10)
+
+     # * Changes the state of the buttons based on the state of the state machine to prevent them from being pressed at the wrong times
+     def update_buttons(self):
+          if(self.sm.current_state.id == "ground"):
+               self.startbtn["state"] = "normal"
+               self.stopbtn["state"] = "disabled"
+          if(self.sm.current_state.id == "starting") | (self.sm.current_state.id == "started"):
+               self.startbtn["state"] = "disabled"
+               self.stopbtn["state"] = "normal"
+          if (self.sm.current_state.id == "stopped"):
+               self.startbtn["state"] = "normal"
+               self.stopbtn["state"] = "disabled"
+          if (self.sm.current_state.id == "stopping") | (self.sm.current_state.id == "curtainTripped"):
+               self.startbtn["state"] = "disabled"
+               self.stopbtn["state"] = "disabled"
+
+     # * Sets the background to orange if in a fault state and resets it if not
+     def update_background(self):
+          if(self.sm.current_state.id == "curtainTripped"):
+               self.root.config(bg="#CB6015")
+               self.frame.config(bg="#FAF9F6")
+               self.label.config(bg="#FAF9F6")
+               messagebox.showerror(message="The light curtain has been tripped. The area around the robot must be cleared. Then you can reset it by pressing the small red square button on the control pannel.")
+          if(self.sm.current_state.id == "stopped"):
+               self.root.config(bg="#99D6EA")
+               self.frame.config(bg="lightblue")
+               self.label.config(bg="lightblue")
 
      # * Cyles the state machine and updates the label
      def cycle(self):
